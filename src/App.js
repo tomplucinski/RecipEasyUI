cf58916f0c1350e4c525a9c9c3f9844a6b788d36
Tom Plucinski <tapluci1@gmail.com>
2021-01-17 13:13:56 -0600

    Created Navbar and Landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing'
 // // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -14,6 +15,7 @@ const App = () => {
       <Router>
         <div>
           <Navbar />
+          <Route exact path="/" component={Landing} />
         </div>
       </Router>
     </Provider>
