2517d50e8fa88b368c8e59911ebe89c7c7257d00
Tom <tapluci1@gmail.com>
2021-03-13 18:12:03 -0600

    Updated dashboard display

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,24 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Navbar from '../Navbar/Navbar'
-import AddRecipe from '../AddRecipe/AddRecipe'
 import './Dashboard.css'
+import { Link } from 'react-router-dom';
 
 const Dashboard = ({ auth: { user } }) => {
 
   return (
-    <div>
+    <div className="dashboard">
       <Navbar />
       <div className="dashboard-header">
         <h2 className="text-primary">Welcome back, {user && user.firstName}!</h2>
-        <AddRecipe />
-        <div className="vegan-recipes">Vegan Recipes</div>
-        <div className="asian-recipes">Asian Recipes</div>
-        <div className="american-recipes">American Recipes</div>
-        <div className="italian-recipes">Italian Recipes</div>
-        <div className="greek-recipes">Greek Recipes</div>
-        <div className="mexican-recipes">Mexican Recipes</div>
+        <Link className="btn btn-primary" to="/add_recipe">Add New Recipe</Link>
+      </div>
+      <div className="vegan-recipes">
+        <div className="dark-overlay-dashboard">
+          <h2 className="text-recipes">Vegan Recipes</h2>
+        </div>
+      </div>
+      <div className="asian-recipes">
+        <div className="dark-overlay-dashboard">
+          <h2 className="text-recipes">Asian Recipes</h2>
+        </div>
+      </div>
+      <div className="american-recipes">
+        <div className="dark-overlay-dashboard">
+          <h2 className="text-recipes">American Recipes</h2>
+        </div>
+      </div>
+      <div className="italian-recipes">
+        <div className="dark-overlay-dashboard">
+          <h2 className="text-recipes">Italian Recipes</h2>
+        </div>
+      </div>
+      <div className="mediterranean-recipes">
+        <div className="dark-overlay-dashboard">
+          <h2 className="text-recipes">Mediterranean Recipes</h2>
+        </div>
+      </div>
+      <div className="mexican-recipes">
+        <div className="dark-overlay-dashboard">
+          <h2 className="text-recipes">Mexican Recipes</h2>
+        </div>
       </div>
     </div>
   );
